aaa4f0d9ac1c5763704c469339b249edd45bbf92
Natan Cardoso <natansc73@gmail.com>
2022-10-12 15:40:54 -0300

    Build First Page with Styles and Fix Test

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,1 +1,15 @@
-export const Main = () => <h1>oi</h1>
+import * as S from './styles'
+
+export const Main = () => (
+  <S.Wrapper>
+    <S.Logo src="#" alt="Image de um átomo e React Avançado escrito ao lado." />
+    <S.Title>React Hard</S.Title>
+    <S.Description>
+      TypeScript, ReactJS, NextJS, e Styled Components
+    </S.Description>
+    <S.Illustration
+      src="#"
+      alt="Um desenvolvedor de frente para um tela com código."
+    />
+  </S.Wrapper>
+)
--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -6,8 +6,16 @@ describe('<Main />', () => {
   it('should render the heading', () => {
     const { container } = render(<Main />)
 
-    expect(screen.getByRole('heading', { name: /oi/i })).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: /React Hard/i })
+    ).toBeInTheDocument()
 
     expect(container.firstChild).toMatchSnapshot()
+  })
+
+  it('should render the colors correctly', () => {
+    const { container } = render(<Main />)
+
+    expect(container.firstChild).toHaveStyle({ 'background-color': '#06092b' })
   })
 })
